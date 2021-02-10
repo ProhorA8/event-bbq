47fff082efcd0e16bce6d4d6a91f6f0466b617f1
ProhorA8 <Procustom4@gmail.com>
2021-02-10 21:53:16 +0300

    change return address of all letters, change style navbar

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   # обратный адрес всех писем по умолчанию
-  default from: 'procustom4@gmail.com'
+  default from: 'gp.emerald2020@gmail.com'
 
   # Задаем макет для всех писем
   layout 'mailer'
