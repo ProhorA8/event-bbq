21770b987fef3f44c9bd491a70ed9ce6956aed03
ProhorA8 <Procustom4@gmail.com>
2021-02-19 12:22:01 +0300

    enable compression js

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,6 +65,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Сжатие JavaScript
+  config.assets.js_compressor = :uglifier
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
